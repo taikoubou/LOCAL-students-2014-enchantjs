4c0a7c74bd45157d543bf3c5e9bfcc861eeb6307
taikoubou <godon.lykn.ows03@gmail.com>
2014-08-09 22:03:57 +0900

    Move char

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
+// 1:UP
+// 2:LEFT
+// 3:RIGHT
+// 4:DOWN
+
 enchant();
 
 window.onload = function() {
-    var game = new Game(320,320);
+    var game = new Core(320,320);
+    game.fps = 60;
+    game.preload('chara1.png','map0.png');
     game.onload = function(){
-        //
+        var bear = new Sprite(32,32);
+
+        bear.image = game.assets['chara1.png'];
+
+        bear.x=0;
+        bear.y=0;
+        bear.frame = [0,0,0,0,0,0,1,1,1,1,1,1];
+
+        bear.addEventListener('enterframe',function(){
+            if(game.input.left)this.x-=2;
+            if(game.input.right)this.x+=2;
+            if(game.input.up)this.y-=2;
+            if(game.input.down)this.y+=2;
+        });
+
+        game.rootScene.addChild(bear);
     };
     game.start();
-}
+};
