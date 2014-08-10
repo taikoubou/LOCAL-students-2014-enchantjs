f06c71b1dac4c92deac86a4bc08ab10728194f6b
taikoubou <godon.lykn.ows03@gmail.com>
2014-08-10 15:49:19 +0900

    Ver. 1.00

--- main.js
+++ main.js
@@ -9,8 +9,9 @@ window.onload = function() {
     items[3] = "小並感";
     items[4] = "あれ";
     items[5] = "ぷよぽっぷ";
+    var item_score = [5,80,1,40,20,100];
     game.fps = 60;
-    game.preload('chara1.png','map0.gif','mbox.png');
+    game.preload('chara1.png','map0.gif','mbox.png','result.png');
     game.keybind(90,'a');
 
     var haveItem = function(){
@@ -18,10 +19,61 @@ window.onload = function() {
         var itemName="";
     }
 
+    var HeroItem = new haveItem();
+    var bear = new Sprite(32,32);
+
     var addItem = function(item,str){
         if(item.itemName != "")item.itemName = str;
-        console.log(typeof(item.n));
         item.n++;
+    }
+
+    var sumScore = function(){
+        var score = 0;
+        var len = HeroItem.length;
+
+        for(var i=0;i<len;i++){
+            for(var j=0;j<6;j++){
+                if(HeroItem[i].itemName == items[j]){
+                    score += item_score[j] * HeroItem[i].n;
+                }
+            }
+        }
+        return score;
+    }
+
+    var ResultScene = function(score){
+        var scene = new Scene();
+        var label = new Label();
+        var bg = new Sprite(320,320);
+
+        bg.image = game.assets['result.png'];
+        bg.x = 0;
+        bg.y = 0;
+        bg.opcity = 0.7;
+
+        label.color = 'black';
+        label.x = 160;
+        label.y = 240;
+        label.font = '32px "Arial"';
+        label.text = score.toString();
+
+        scene.addEventListener('enterframe',function(){
+            var len = HeroItem.length;
+
+            if(game.input.a){
+                for(var i=0;i<len;i++){
+                    HeroItem.pop();
+                }
+                bear.x = 16;
+                bear.y = 16;
+                game.popScene();
+            }
+        });
+
+        scene.addChild(bg);
+        scene.addChild(label);
+
+        return scene;
     }
 
     var MessageScene = function(randN){
@@ -51,8 +103,6 @@ window.onload = function() {
         var buttonflag = false;
         var len = itemList.length;
 
-        console.log(itemList[0]);
-
         label = new Array();
 
         var sw = new MenuWindow(scene,10,230);
@@ -71,7 +121,6 @@ window.onload = function() {
             label[i] = (function(n,lb,item){
                 var dy = [240,240,260,260,280,280];
                 var dx = [20,170,20,170,20,170];
-                console.log(typeof(lb));
                 lb.y = dy[n];
                 lb.x = dx[n];
                 lb.font = '16px "Arial"';
@@ -128,9 +177,7 @@ window.onload = function() {
         var messageFlag = false;
         var buttonFlag = false;
         var randN = 0;
-        var bear = new Sprite(32,32);
         var map1 = new Map(16,16);
-        var HeroItem = new haveItem();
         HeroItem = new Array();
 
         bear.scale(0.5,0.5);
@@ -146,6 +193,9 @@ window.onload = function() {
 
         bear.addEventListener('enterframe',function(){
             var itemEncflag = false;
+            if(this.x >= 214 && this.x <= 222 && this.y >= 86 && this.y <= 94){
+                game.pushScene(ResultScene(sumScore()));
+            }
             if(game.input.left && !map1.hitTest(this.x-2,this.y)){
                 this.x-=2;
                 itemEncflag = isEncounter();
@@ -194,9 +244,7 @@ window.onload = function() {
                     var hoge = new haveItem();
                     HeroItem[len] = new haveItem();
                     HeroItem[len].n = 0;
-                    console.log(HeroItem[len]);
                     addItem(HeroItem[len],items[randN]);
-                    console.log(HeroItem[len]);
                 }
                 else flag =false;
                 game.pushScene(MessageScene(randN));
